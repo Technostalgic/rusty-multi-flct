15a402b629ddd7245abfa739b14f95fca9288c59
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-09-01 01:59:42 -0600

    implement loading collection

--- rust-core/src/io.rs
+++ rust-core/src/io.rs
@@ -1,18 +1,21 @@
 use std::{
+    fs::read_dir,
     io::{Error, ErrorKind, Result},
     path::{Path, PathBuf},
+    slice::Iter,
 };
 
 // DEFINITIONS -----------------------------------------------------------------
 
-/// Collection of filesystem metadata objects
-pub struct MetaCollection<'a> {
-    meta_files: Vec<&'a MetaFile>,
-}
-
 /// Individual metadata object on a filesystem
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct MetaFile {
     path: PathBuf,
+}
+
+/// Collection of filesystem metadata objects
+pub struct MetaCollection {
+    meta_files: Box<[MetaFile]>,
 }
 
 /// Exposes metadata required for processing on related objects
@@ -38,7 +41,7 @@ impl MetaFile {
     ///
     /// Returns [`Ok`] if the file can be loaded, otherwise an [`Error`]
     /// explaining why it cannot be loaded.
-    pub fn load<S: AsRef<Path>>(path: S) -> Result<MetaFile> {
+    pub fn load(path: impl AsRef<Path>) -> Result<MetaFile> {
         let path = path.as_ref();
         if path.exists() {
             Ok(MetaFile {
@@ -61,12 +64,39 @@ impl Metadata for MetaFile {
     }
 }
 
+impl MetaCollection {
+    pub fn load(path: impl AsRef<Path>) -> Result<MetaCollection> {
+        let mut metafiles: Vec<MetaFile> = Vec::new();
+        for entry in read_dir(&path)? {
+            let entry = entry?;
+            if !entry.file_type()?.is_file() {
+                continue;
+            }
+            metafiles.push(MetaFile::load(&path)?);
+        }
+        metafiles.sort();
+        Ok(MetaCollection {
+            meta_files: metafiles.into_boxed_slice(),
+        })
+    }
+
+    /// Iterator that allows iteration over metadata in the collection
+    pub fn iter(&self) -> Iter<'_, MetaFile> {
+        self.meta_files.iter()
+    }
+
+    /// Number of metadatas in the collection
+    pub fn count(&self) -> usize {
+        self.meta_files.len()
+    }
+}
+
 // UNIT TESTS ------------------------------------------------------------------
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::path::PathBuf;
+    use std::{fs, path::PathBuf};
 
     const TEST_DATA: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/");
 
@@ -84,9 +114,23 @@ mod tests {
 
     #[test]
     fn load_meta_dir() {
-        todo!();
         // load dir path
         // create metadata collection from metas in dir
         // check metadata collection length to match number of meta files in dir
+        let mut paths: Vec<PathBuf> = Vec::new();
+        for entry in fs::read_dir(TEST_DATA).unwrap() {
+            let entry = entry.unwrap();
+            if !entry.file_type().unwrap().is_file() {
+                continue;
+            }
+            paths.push(entry.path());
+        }
+        paths.sort();
+        let paths = paths;
+
+        let collection = MetaCollection::load(TEST_DATA).unwrap();
+        let cc = collection.count();
+        let pc = paths.len();
+        assert!(collection.count() == paths.len());
     }
 }
