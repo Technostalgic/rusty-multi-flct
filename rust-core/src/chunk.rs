3aa378aa1532ea67a5f8f7cd0f6c74b1640a7302
Isaiah Smith <isaiahnsmith@hotmail.com>
2025-09-22 00:11:46 -0600

    implement 2d vectors in their own crate and implement other arithmetic ops

--- rust-core/src/chunk.rs
+++ rust-core/src/chunk.rs
@@ -1,60 +1,6 @@
-use ndarray::{Array2, ArrayBase, ArrayView2, Data, IntoDimension, Ix2, s};
-use std::ops::{Add, Div, Mul, Sub};
-
-// DEFINITIONS -----------------------------------------------------------------
-
-trait Floaty: Clone + Copy + PartialEq + Add + Sub + Mul + Div {}
-impl<T: Clone + Copy + PartialEq + Add + Sub + Mul + Div> Floaty for T {}
-
-trait Inty: Clone + Copy + PartialEq + Eq + Add + Sub + Mul + Div + TryInto<usize> {}
-impl<T: Clone + Copy + PartialEq + Eq + Add + Sub + Mul + Div + TryInto<usize>> Inty for T {}
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-struct Vec2f<F = f32>
-where
-    F: Floaty,
-{
-    x: F,
-    y: F,
-}
-
-#[derive(Clone, Copy, Hash, PartialEq, Eq)]
-struct Vec2i<I = i32>
-where
-    I: Inty,
-{
-    x: I,
-    y: I,
-}
+use ndarray::{Array2, ArrayBase, ArrayView2, Data, Ix2, s};
 
 // IMPLEMENTATIONS -------------------------------------------------------------
-
-impl<F> Add for Vec2f<F>
-where
-    F: Floaty + Add<Output = F>,
-{
-    type Output = Self;
-
-    fn add(self, rhs: Vec2f<F>) -> Self::Output {
-        Vec2f::<F> {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
-}
-
-impl<I> From<Ix2> for Vec2i<I>
-where
-    I: Inty + From<usize>,
-{
-    fn from(value: Ix2) -> Self {
-        let dim = value.into_dimension();
-        Vec2i::<I> {
-            x: dim[0].into(),
-            y: dim[1].into(),
-        }
-    }
-}
 
 /// Slice data into array view kernels of specified size and spacing.
 ///
@@ -100,7 +46,6 @@ mod tests {
 
     #[test]
     fn chunk_32() {
-
         // create an array and chunk it into kernels of size 32
         let arr = Array2::from_shape_fn((256, 128), |(x, y)| x + y * 256);
         assert_eq!(arr.shape(), &[256, 128]);
---
+++ rust-core/src/vec.rs
@@ -0,0 +1,145 @@
+use ndarray::{IntoDimension, Ix2};
+use std::ops::{Add, Div, Mul, Sub};
+
+// DEFINITIONS -----------------------------------------------------------------
+
+trait Floaty: Clone + Copy + PartialEq + Add + Sub + Mul + Div {}
+impl<T: Clone + Copy + PartialEq + Add + Sub + Mul + Div> Floaty for T {}
+
+trait Inty: Clone + Copy + PartialEq + Eq + Add + Sub + Mul + Div + TryInto<usize> {}
+impl<T: Clone + Copy + PartialEq + Eq + Add + Sub + Mul + Div + TryInto<usize>> Inty for T {}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct Vec2<F = f64>
+where
+    F: Floaty,
+{
+    x: F,
+    y: F,
+}
+
+// IMPLEMENTATIONS -------------------------------------------------------------
+
+impl<F> Vec2<F>
+where
+    F: Floaty,
+{
+    pub fn new(x: F, y: F) -> Self {
+        Vec2 { x: x, y: y }
+    }
+}
+
+impl<I> From<Ix2> for Vec2<I>
+where
+    I: Inty + From<usize>,
+{
+    fn from(value: Ix2) -> Self {
+        let dim = value.into_dimension();
+        Vec2::<I> {
+            x: dim[0].into(),
+            y: dim[1].into(),
+        }
+    }
+}
+
+impl<F> Add for Vec2<F>
+where
+    F: Floaty + Add<Output = F>,
+{
+    type Output = Self;
+    fn add(self, rhs: Vec2<F>) -> Self::Output {
+        Self::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl<F> Sub for Vec2<F>
+where
+    F: Floaty + Sub<Output = F>,
+{
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+impl<F> Mul for Vec2<F>
+where
+    F: Floaty + Mul<Output = F>,
+{
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::new(self.x * rhs.x, self.y * rhs.y)
+    }
+}
+
+impl<F> Mul<F> for Vec2<F>
+where
+    F: Floaty + Mul<Output = F>,
+{
+    type Output = Self;
+    fn mul(self, rhs: F) -> Self::Output {
+        Self::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl<F> Div for Vec2<F>
+where
+    F: Floaty + Div<Output = F>,
+{
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::new(self.x / rhs.x, self.y / rhs.y)
+    }
+}
+
+impl<F> Div<F> for Vec2<F>
+where
+    F: Floaty + Div<Output = F>,
+{
+    type Output = Self;
+    fn div(self, rhs: F) -> Self::Output {
+        Self::new(self.x / rhs, self.y / rhs)
+    }
+}
+
+// UNIT TESTS ------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn vec_add() {
+        assert_eq!(Vec2::new(4, 15), Vec2::new(3, 8) + Vec2::new(1, 7));
+        assert_eq!(
+            Vec2::new(4.2, 15.2),
+            Vec2::new(3.1, 8.1) + Vec2::new(1.1, 7.1)
+        );
+    }
+
+    #[test]
+    fn vec_sub() {
+        assert_eq!(
+            Vec2::new(2.0, 1.0),
+            Vec2::new(3.1, 8.1) - Vec2::new(1.1, 7.1)
+        );
+    }
+
+    #[test]
+    fn vec_mul() {
+        assert_eq!(Vec2::new(10.0, 25.0), Vec2::new(1.0, 2.5) * 10.0);
+        assert_eq!(
+            Vec2::new(4.0, 7.5),
+            Vec2::new(2.0, 2.5) * Vec2::new(2.0, 3.0)
+        );
+    }
+
+    #[test]
+    fn vec_div() {
+        assert_eq!(Vec2::new(0.1, 2.5), Vec2::new(1.0, 25.0) / 10.0);
+        assert_eq!(
+            Vec2::new(0.5, 7.5 / 3.0),
+            Vec2::new(2.0, 7.5) / Vec2::new(4.0, 3.0)
+        );
+    }
+}
